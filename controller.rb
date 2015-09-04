919d1c01b896a24426fab62931edbe0dc4c498b5
Sarah Glaum & Dian Pan & David Tu <pair+SarahCG.dianpan.spacebear5000@devbootcamp.com>
2015-09-04 17:24:46 -0400

    clean up code

--- controller.rb
+++ controller.rb
@@ -5,31 +5,25 @@ require_relative 'app/view'
 require_relative 'model'
 
 
+
 class Controller
   include View
-  #unless game over? (model)
-  #shift the first question & answer (model)
-  #randomly select 2 dummy answers (model)
-  #display to user (view)
-  #user will input a,b,or c (controller)
-  #we will tell them if they are correct or not (view)
-
   attr_accessor :joke_gen
   def initialize
-    # p Joke.all
     @joke_gen = JokeGenerator.new(all_jokes: Joke.all.to_a, all_dummy: Dummy.all.to_a )
   end
 
   def run
+    system("clear")
     View.intro
     puts "\n"
-    View.question_prompt
     until joke_gen.game_over?
       curr_joke = @joke_gen.joke_getter
       puts curr_joke.question
       puts "a) #{curr_joke.answer}"
       puts "b) #{joke_gen.dummy_sample}"
       puts "c) #{joke_gen.dummy_sample}"
+      View.question_prompt
       if View.get_user_input == "a"
         View.correct
         system("clear")
@@ -37,8 +31,8 @@ class Controller
         View.incorrect
       end
     end
+    View.game_over
   end
-
 end
 
 
--- runner.rb
+++ runner.rb
@@ -1,9 +1,5 @@
-require_relative 'config/environment'
-
 # Do not log database activity when running our application
 ActiveRecord::Base.logger = nil
 
 # Enter your code below ...
 
-# new_game = Controller.new
-# new_game.run
